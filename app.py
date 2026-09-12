bc759ec2e438a7fa162101a3e3acc3afbbec35d2
syssefim <serafim.sharkov@gmail.com>
2026-09-11 20:15:51 -0700

    Basic script to text voice

---
+++ app.py
@@ -0,0 +1,15 @@
+from elevenlabs import ElevenLabs
+from elevenlabs.play import play
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+eleven_labs_api = os.getenv("ELEVEN_LABS_API_KEY")
+
+client = ElevenLabs(api_key=eleven_labs_api)
+audio = client.text_to_speech.convert(
+    voice_id="JBFqnCBsd6RMkjVDRZzb",
+    text="I have no strings to hold me down",
+    model_id="eleven_multilingual_v2",
+)
+play(audio)
